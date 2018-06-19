5890589e39c765f85294057dc13f4b7ac8e8e2d2
raelAndrade <israelg.andrade@bol.com.br>
2018-06-19 16:39:56 -0300

    Add api-doc

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,5 @@
 import bodyParser from "body-parser";
+import express from "express";
 
 module.exports = app => {
 	app.set("port", 3000);
@@ -8,5 +9,6 @@ module.exports = app => {
 	app.use((req, res, next) => {
 		delete req.body.id;
 		next();
-	});	
+	});
+	app.use(express.static("public"));
 };
---
+++ public/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Documentação - Node Task API",
+  "template": {
+    "forceLanguage": "pt_br"
+  },
+  "version": "1.0.0",
+  "description": "API de gestão de tarefas",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-06-19T19:34:06.781Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
