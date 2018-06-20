711a66ad47e40e19edea584a07cb0c157bc88c2d
raelAndrade <israelg.andrade@bol.com.br>
2018-06-20 09:52:01 -0300

    Adicionando API-DOC

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,6 +3,32 @@ module.exports = app => {
 
 	app.route("/tasks")
 		.all(app.auth.authenticate())
+		/**
+		* @api {get} /tasks Lista tarefas
+		* @apiGroup Tarefas
+		* @apiHeader {String} Authorization Token de usuário
+		* @apiHeaderExample {json} Header
+		* 	{"Authorization": "JWT xyz.abc.123.hgf"}
+		* @apiSuccess {Object[]} tasks Lista de tarefas
+		* @apiSuccess {Number} tasks.id Id de registro
+		* @apiSuccess {String} tasks.title Título da tarefa
+		* @apiSuccess {Boolean} tasks.done Tarefa foi concluída?
+		* @apiSuccess {Date} tasks.updated_at Data de atualização
+		* @apiSuccess {Date} tasks.created_at Data de cadastro
+		* @apiSuccess {Number} tasks.user_id Id do usuário
+		* @apiSuccessExample {json} Sucesso
+		*  	HTTP/1.1 200 OK
+		*  	[{
+		*  		"id": 1,
+		*  		"title": "Estudar",
+		*  		"done": false
+		*  		"updated_at": "2015-09-24T15:46:51.778Z",
+		*  		"created_at": "2015-09-24T15:46:51.778Z",
+		*  		"user_id": 1
+		*  	}]
+		* @apiErrorExample {json} Erro de consulta
+		*  HTTP/1.1 412 Precondition Failed
+		*/
 		.get((req, res) => {
 			// "/tasks": Lista tarefas
 			Tasks.findAll({
@@ -15,6 +41,35 @@ module.exports = app => {
 				});
 
 		})
+
+		/**
+		* @api {post} /tasks Cadastra uma tarefa
+		* @apiGroup Tarefas
+		* @apiHeader {String} Authorization Token de usuário
+		* @apiHeaderExample {json} Header
+		* 	{"Authorization": "JWT xyz.abc.123.hgf"}
+		* @apiParam {String} title Título da tarefa
+		* @apiParamExample {json} Entrada
+		* 	{"title": "Estudar"}
+		* @apiSuccess {Number} id Id de registro
+		* @apiSuccess {String} title Título da tarefa
+		* @apiSuccess {Boolean} done=false Tarefa foi concluída?
+		* @apiSuccess {Date} updated_at Data de atualização
+		* @apiSuccess {Date} created_at Data de cadastro
+		* @apiSuccess {Number} user_id Id do usuário
+		* @apiSuccessExample {json} Sucesso
+		*  	HTTP/1.1 200 OK
+		*  	[{
+		*  		"id": 1,
+		*  		"title": "Estudar",
+		*  		"done": false
+		*  		"updated_at": "2015-09-24T15:46:51.778Z",
+		*  		"created_at": "2015-09-24T15:46:51.778Z",
+		*  		"user_id": 1
+		*  	}]
+		* @apiErrorExample {json} Erro de consulta
+		*  HTTP/1.1 412 Precondition Failed
+		*/
 		.post((req, res) => {
 			// "/tasks": Cadastrar uma nova tarefa
 			req.body.user_id = req.user.id;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,26 @@ module.exports = app => {
     
     const Users = app.db.models.Users;
 
+    /**
+     * @api {get} /user Exibe usuário autenticado
+     * @apiGroup Usuário
+     * @apiHeader {String} Authorization Token de usuário
+     * @apiHeaderExample {json} Header
+     *  {"Authorization": "JWT xyz.abc.123.hgf"}
+     * @apiSuccess {Number} id Id de registro
+     * @apiSuccess {String} name Name 
+     * @apiSuccess {String} email Email
+     * @apiSuccessExample {json} Sucesso
+     *  HTTP/1.1 200 OK
+     *  {
+     *      "id": 1,
+     *      "name": "John Connor",
+     *      "email": john@connor.net
+     *  }
+     * @apiErrorExample {json} Erro de Consulta
+     *  HTTP/1.1 412 Precondition Failed
+     */
+
     app.route("/user")
         .all(app.auth.authenticate())
         .get((req, res) => {
@@ -13,6 +33,17 @@ module.exports = app => {
                 res.status(412).json({msg: error.message});
             });
         })
+        /**
+         * @api {delete} /user Excluir usuário autenticado
+         * @apiGroup Usuário
+         * @apiHeader {String} Authorization Token de usuário
+         * @apiHeaderExample {json} Header
+         *  {"Authorization": JWT xyz.abc.123.hgf}
+         * @apiSuccessExample {json} Sucesso
+         *  HTTP/1.1 204 No Content
+         * @apiErrorExample {json} Erro na exclusão
+         *  HTTP/1.1 412 Precondition Failed
+         */
         .delete((req, res) => {
             Users.destroy({where: {id: req.user.id} })
                 .then(result => res.sendStatus(204))
@@ -21,7 +52,36 @@ module.exports = app => {
                 });
         });
 
-    // Essa rota não precisa de autenticação
+    /**
+     * @api {post} /users Cadastra novo usuaŕio
+     * @apiGroup Usuário
+     * @apiParam {String} name Nome
+     * @apiParam {String} email Email
+     * @apiParam {String} password Senha
+     * @apiParamExample {json} Entrada
+     *  {
+     *      "name": "John Connor",
+     *      "email": "john@connor.net",
+     *      "password": "12345"
+     *  }
+     * @apiSuccess {Number} id ID de registro
+     * @apiSuccess {String} name Nome
+     * @apiSuccess {String} email Email
+     * @apiSuccess {String} password Senha criptografada
+     * @apiSuccess {Date} updated_at Data de atualização
+     * @apiSuccess {Date} created_at Data de cadastro
+     * @apiSuccessExample {json} Sucesso
+     *  HTTP/1.1 200 OK
+     *  {
+     *      "id": 1,
+     *      "name": "John Connor",
+     *      "email": "john@connor.net",
+     *      "password": "a$10$SK1B1",
+     *      "updated_at": "2018-06-19T23:29:51.778Z",
+     *      "created_at": "2018-06-19T23:29:51.778Z"
+     *  }
+     * @apiErrorExample {json} Erro no cadastro
+     */
     app.post("/users", (req, res) => {
         Users.create(req.body)
             .then(result => res.json(result))
